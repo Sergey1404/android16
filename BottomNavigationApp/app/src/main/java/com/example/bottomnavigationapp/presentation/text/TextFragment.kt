44e6657d32802161e7b7c4f253674e9d06cb9cb5
Sergey1404 <93861834+Sergey1404@users.noreply.github.com>
2023-05-15 18:32:06 +0700

    Add files via upload

---
+++ BottomNavigationApp/app/src/main/java/com/example/bottomnavigationapp/presentation/text/TextFragment.kt
@@ -0,0 +1,7 @@
+package com.example.bottomnavigationapp.presentation.text
+
+import androidx.fragment.app.Fragment
+import com.example.bottomnavigationapp.R
+
+class TextFragment : Fragment(R.layout.fragment_text) {
+}
